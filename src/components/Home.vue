eaf5d5e8f04e8a95839d570e5ca6f1e740ed9555
zhangxiaoA1 <2530558138@qq.com>
2022-12-23 18:51:35 +0800

    12.23ceshi

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>首页</h1>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped="css">
+
+</style>
